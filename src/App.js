a1f9358dcb66417dc9bb3e7c21c8e3ef86d2f6ae
flatironschool <t.benson@live.co.uk>
2020-02-25 18:33:11 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import Header from './components/Header'
 import MainContainer from './containers/MainContainer'
 
 class App extends Component {
+// move the mainContainer as we won't need to pass Stocks to header - no point passing it down any further than need to
+  // don't need to use constructor anymore can just declare state =
+  
+
+ 
+
   render() {
     return (
       <div>
-        <Header/>
-        <MainContainer/>
+        <Header />
+        <MainContainer />
       </div>
     );
   }

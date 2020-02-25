307ce09b94ba341c167c7dbf393d9dffa3e7e28d
RogeredBacon <jinglis12@googlemail.com>
2020-02-25 15:12:30 +0000

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-const Stock = () => (
-  <div>
-
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
-      </div>
-    </div>
-
-
-  </div>
+const Stock = props => (
+	<div onClick={() => props.portfolioFunction(props.id)}>
+		<div className='card'>
+			<div className='card-body'>
+				<h5 className='card-title'>{props.name}</h5>
+				<p className='card-text'>{//ticker: stock price
+				`${props.ticker}: ${props.price}`}</p>
+			</div>
+		</div>
+	</div>
 );
 
-export default Stock
+export default Stock;
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import Stock from '../components/Stock'
+import Stock from '../components/Stock';
 
 class PortfolioContainer extends Component {
-
-  render() {
-    return (
-      <div>
-        <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
-      </div>
-    );
-  }
-
+	render() {
+		return (
+			<div>
+				<h2>My Portfolio</h2>
+				{this.props.portfolio.map((stock, index) => (
+					<Stock
+						id={stock.id}
+						key={stock.id}
+						ticker={stock.ticker}
+						price={stock.price}
+						name={stock.name}
+						portfolioFunction={this.props.removeFromPortfolio}
+					/>
+				))}
+			</div>
+		);
+	}
 }
 
 export default PortfolioContainer;
